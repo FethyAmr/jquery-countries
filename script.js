dd36167e4c2fb466782875924c7adac72dd9f1ad
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-06 17:41:46 +0200

    scdCommit

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function getCountries()
 
-$.ajax({
-    url: 'http://localhost:8000/countries',
-    success: function(data, statuts, response) {
-        console.log(data[0].name);
-        console.log(data[0].capital);
-    }
-});
+$(function(){
+
+    $("button").click(function(){
+
+        $.ajax({
+            url: "http://localhost:8000/countries",
+            success: function (data, status, response) {
+                // return data
+                $("#country-1").html(data[0])
+                $("#country-2").html(data[1])
+                $("#country-3").html(data[2])
+                $("#country-4").html(data[3])
+                $("#country-5").html(data[4])
+                // $("#okkk").html(data)
+            }
+        })
+
+
+
+
+    })
+})
